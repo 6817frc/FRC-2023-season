d5d3a9b1f3ca665846df19ed8256ab4810d73928
6817frc <56094422+6817frc@users.noreply.github.com>
2022-12-05 16:59:59 -0600

    Added old base code which "should" work

---
+++ old robot code/Basic/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,64 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.lang.Math;
+import edu.wpi.first.wpilibj.IterativeRobotBase;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.MotorSafety;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+
+/**
+ * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
+ * arcade steering.
+ */
+public class Robot extends TimedRobot {
+  private final Spark leftFront = new Spark(0); //variable for front left motor
+  private final Spark leftBack = new Spark(1);
+  private final Spark rightFront = new Spark(3);
+  private final Spark rightBack = new Spark(2);
+  private final MotorControllerGroup leftGroup = new MotorControllerGroup(leftFront, leftBack);
+  private final MotorControllerGroup rightGroup = new MotorControllerGroup(rightFront, rightBack);
+  private final DifferentialDrive robotDrive = new DifferentialDrive (leftGroup, rightGroup);
+  private final Joystick m_stick = new Joystick(0);
+  private final XboxController logiController = new XboxController(1); // 1 is the USB Port to be used as indicated on the Driver Station
+  private NetworkTable datatable;
+  String autoName="low goal";
+  @Override
+  public void robotInit() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    datatable = inst.getTable("datatable");
+    rightBack.setInverted(true);
+    rightFront.setInverted(true);
+    robotDrive.setSafetyEnabled(true);
+    
+  }
+  @Override
+  public void teleopInit () {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // Drive with arcade drive.
+    // That means that the Y axis drives forward
+    // and backward, and the X turns left and right.
+    robotDrive.arcadeDrive((-logiController.getRawAxis(5)),(logiController.getRawAxis(4)/1.5));
+   
+  }
+}
---
+++ old robot code/README.md
@@ -0,0 +1,14 @@
+# FRC-2022
+
+Objectives for Utils:
+
+1. ***Camera Trainer***
+   Obj:*Using Python you will need to create a program*
+   *that can return the RGB value of an image at any given point.*
+   Details:
+2. ***Target Dialer***
+   Obj:*Create knobs/sliders/buttons to adjust values*
+   *of detecting retro-reflective targets, based on psuedo code*
+3. ***Deadzone Finder***
+   Obj:*Create a program that records the axis and buttons*
+   *of a given controller to be able to label and use them as needed*
